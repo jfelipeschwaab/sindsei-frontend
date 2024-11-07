cee5493921b4ef73f5a81c4bc73c487d3ff035b9
João Felipe <joaofsp.games@gmail.com>
2024-11-06 23:47:03 -0300

    chore(Comments): Retirando comentários desnecessários

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,13 +6,13 @@ import EmailList from '../../components/EmailList';
 import EmailDetail from '../../components/EmailDetail';
 import CategoryNav from '../../components/CategoryNav';
 import Header from '../../components/Header';
-import emailsData from '../../data/emailsData'; // Importe o arquivo de dados
+import emailsData from '../../data/emailsData'; 
 import { Email } from '../types/types';
 
 const HomePage: React.FC = () => {
   const [selectedEmail, setSelectedEmail] = useState<Email | null>(null);
   const [selectedCategory, setSelectedCategory] = useState<string>('Todos');
-  const [searchTerm, setSearchTerm] = useState<string>(''); // Estado para o termo de pesquisa
+  const [searchTerm, setSearchTerm] = useState<string>(''); 
 
   const handleSelectEmail = (email: Email) => {
     setSelectedEmail(email);
@@ -20,14 +20,13 @@ const HomePage: React.FC = () => {
 
   const handleSelectCategory = (category: string) => {
     setSelectedCategory(category);
-    setSelectedEmail(null); // Limpa a seleção de email ao mudar de categoria
+    setSelectedEmail(null); 
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  // Filtra os emails com base na categoria e no termo de pesquisa
   const filteredEmails = emailsData
     .filter(email => selectedCategory === 'Todos' || email.tag === selectedCategory)
     .filter(email => email.subject.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -36,13 +35,8 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Header */}
       <Header searchTerm={searchTerm} onSearchChange={handleSearchChange} />
-
-      {/* Categoria */}
       <CategoryNav selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
-
-      {/* Conteúdo principal */}
       <div className="flex flex-1">
         <Sidebar />
         <EmailList emails={filteredEmails} onSelectEmail={handleSelectEmail} />
