08cf2bce848a9516db1869f7058dd716e2033e04
João Felipe <joaofsp.games@gmail.com>
2024-11-11 09:58:10 -0300

    update(Home): Sweet Alert implementado

--- src/components/SignInSignUp.tsx
+++ src/components/SignInSignUp.tsx
@@ -1,7 +1,8 @@
 // components/SignInSignUp.tsx
 "use client";
 import React, { useState } from 'react';
-import { useRouter } from 'next/navigation'; // Importa useRouter para redirecionamento
+import { useRouter } from 'next/navigation'; 
+import Swal from 'sweetalert2'; 
 
 interface SignUpFormData {
   username: string;
@@ -15,7 +16,7 @@ interface LoginFormData {
 }
 
 const SignInSignUp: React.FC = () => {
-  const router = useRouter(); // Define o hook de roteamento
+  const router = useRouter(); 
   const [isRightPanelActive, setIsRightPanelActive] = useState<boolean>(false);
   const [formData, setFormData] = useState<SignUpFormData>({ username: '', email: '', password: '' });
   const [loginData, setLoginData] = useState<LoginFormData>({ email: '', password: '' });
@@ -73,10 +74,16 @@ const SignInSignUp: React.FC = () => {
       }
 
       const data = await response.json();
-      alert('Logged in successfully');
+      
+      await Swal.fire({
+        title: 'Sucesso!',
+        text: 'Logado com sucesso',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+
       console.log(data);
 
-      // Redireciona para a página inicial após login bem-sucedido
       router.push('/home');
     } catch (error) {
       console.error(error);
@@ -88,14 +95,11 @@ const SignInSignUp: React.FC = () => {
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div className={`relative w-[1200px] max-w-full min-h-[700px] bg-white shadow-lg rounded-lg overflow-hidden transition-all duration-700 ease-in-out ${isRightPanelActive ? 'right-panel-active' : ''}`}>
         
-        {/* Container para Cadastro */}
         <div className={`absolute top-0 h-full w-1/2 p-20 transition-all duration-700 ease-in-out ${isRightPanelActive ? 'translate-x-full opacity-100 z-10' : 'opacity-0'}`}>
           <form onSubmit={handleSignUp} className="bg-white flex flex-col items-center justify-center h-full text-center">
             <h1 className="font-bold text-3xl mb-10 text-black">Crie sua Conta</h1>
             <div className="flex gap-2 mb-2">
-              {/* Social media links (placeholders) */}
             </div>
-            {/* <span className="text-sm mb-4">ou</span> */}
             <input type="text" name="username" placeholder="Nome" className="w-full mb-5 p-3 bg-gray-100 rounded-md" onChange={(e) => handleInputChange(e, false)} />
             <input type="email" name="email" placeholder="Email" className="w-full mb-5 p-3 bg-gray-100 rounded-md" onChange={(e) => handleInputChange(e, false)} />
             <input type="password" name="password" placeholder="Senha" className="w-full mb-10 p-3 bg-gray-100 rounded-md" onChange={(e) => handleInputChange(e, false)} />
@@ -103,14 +107,11 @@ const SignInSignUp: React.FC = () => {
           </form>
         </div>
 
-        {/* Container para Login */}
         <div className={`absolute top-0 h-full w-1/2 p-20 transition-all duration-700 ease-in-out ${isRightPanelActive ? 'opacity-0' : 'translate-x-0 opacity-100 z-10'}`}>
           <form onSubmit={handleSignIn} className="bg-white flex flex-col items-center justify-center h-full text-center">
             <h1 className="font-bold text-3xl mb-10 text-black">Faça Login</h1>
             <div className="flex gap-2 mb-4">
-              {/* Social media links (placeholders) */}
             </div>
-            {/* <span className="text-sm mb-4">ou</span> */}
             <input type="email" name="email" placeholder="Email" className="w-full mb-5 p-3 bg-gray-100 rounded-md" onChange={(e) => handleInputChange(e, true)} />
             <input type="password" name="password" placeholder="Senha" className="w-full mb-10 p-3 bg-gray-100 rounded-md" onChange={(e) => handleInputChange(e, true)} />
             <a href="#" className="text-gray-500 mb-10">Esqueceu sua senha?</a>
@@ -118,7 +119,6 @@ const SignInSignUp: React.FC = () => {
           </form>
         </div>
 
-        {/* Overlay Container */}
         <div className={`absolute top-0 left-1/2 h-full w-1/2 bg-[#202020] text-white flex items-center justify-center overflow-hidden transition-transform duration-700 ${isRightPanelActive ? 'translate-x-[-100%]' : ''}`}>
           <div className="w-2/3 text-center">
             <h1 className="text-3xl font-bold mb-20">{isRightPanelActive ? 'Bem-Vindo de Volta!' : 'Comece Agora!'}</h1>
